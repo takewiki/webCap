81375975f98123c8667b4e8573934e7ab2f18d35
takewiki <hulilei@takewiki.com.cn>
2019-03-26 12:48:54 +0800

    update webpage

--- R/web.R
+++ R/web.R
@@ -8,9 +8,11 @@
 #' @export
 #'
 #' @examples web_page()
-web_page <- function(url="https://search.51job.com/list/020000,000000,0000,00,9,99,%25E6%2595%25B0%25E6%258D%25AE%25E7%25A7%2591%25E5%25AD%25A6,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=4&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=") {
+web_page <- function(url="https://search.51job.com/list/020000,000000,0000,00,9,99,%25E6%2595%25B0%25E6%258D%25AE%25E7%25A7%2591%25E5%25AD%25A6,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=4&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=",
+                     encoding='UTF-8'
+                     ) {
   
-  res<- read_html(url);
+  res<- read_html(url,encoding = encoding);
   # class(res) <-'web_page'
   return(res)
 }
